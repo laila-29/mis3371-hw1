75d620a446cb3fb1e525b6897173986cec01549e
laila-29 <lhunte06@gmail.com>
2026-02-27 01:03:23 -0600

    Add files via upload

---
+++ hw1.js
@@ -0,0 +1,20 @@
+/*
+Name: Laila Hunte
+Date created: 02/12/2026
+Date last edited: 02/12/2026
+Version: 1.0
+Description: Homework 1 JS 
+*/
+
+//dynamic date js code
+const d = new Date();
+let text = d.toLocaleDateString();
+document.getElementById("today").innerHTML = text;
+//name slider js code
+let slider = document.getElementById("range");
+let output = document.getElementById("range-slider");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+};
